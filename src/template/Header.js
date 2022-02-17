06fd077e4a97409f83ad41851ac7cb21d2bd3076
Radgus <dominguezradgus@gmail.com>
2022-02-17 09:05:40 -0600

    para github page

--- src/template/Header.js
+++ src/template/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/">
+          <a href="/spa_js_vanilla/dist/">
             100tifi.co
           </a>
         </h1>
